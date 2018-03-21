a6c6895e5aec6f3f217cc4df7d7e20bc6744c81f
Noé Pion <noe.pion@grenoble-inp.org>
2018-03-21 15:55:04 +0100

    blowjob done

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/9/.base/pionn/home/SEPC/tp_shell/SEPC1
+# Build directory: /user/9/.base/pionn/home/SEPC/tp_shell/SEPC1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(UnitShellTests "../tests/allShellTests.rb")
